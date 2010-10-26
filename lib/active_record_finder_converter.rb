a703cd50e2da2ce47f791223c96e52132a19a861
Peer Allan <peer@canadadrugs.com>
2010-10-27 03:02:08 +0800

    Added .all and .first support with tests

--- lib/active_record_finder_converter.rb
+++ lib/active_record_finder_converter.rb
@@ -18,32 +18,21 @@ module Rails
 
       def parse_file(file)
         raw_ar_finders = ''
-        ["find(:all", "find(:first", "find.*:conditions =>", ":joins =>"].each do |v|
+        ["find(:all", "find(:first", "find.*:conditions =>", ":joins =>", '\.all(', '\.first('].each do |v|
           raw_ar_finders += `grep -r '#{v}' #{file}`
         end
+        
         ar_finders = raw_ar_finders.split("\n").uniq.reject {|l| l.include?('named_scope') }
         
         unless ar_finders.empty?
+          file_contents = File.read(file)
 
           failures = []
           
           new_ar_finders = ar_finders.map do |ar_finder|
             begin
-              full_method, arguments = find_method(ar_finder)
-              case full_method
-              when nil
-                raise RuntimeError, "can't parse due to unmatched braces"
-              when 'find(:all)'
-                new_line = ar_finder.sub(full_method, 'all')
-              when 'find(:first)'
-                new_line = ar_finder.sub(full_method, 'first')
-              else
-                finder_hash = arguments.split(',')
-                all_or_first = finder_hash.shift
-                arel = ArelConverter.translate("{#{finder_hash.join(',')}}")
-                new_line = ar_finder.sub(full_method, arel)
-                new_line += ".first" if all_or_first.include?(':first')
-              end
+              new_line = process_line(ar_finder)
+              file_contents.gsub!(ar_finder, new_line)
               [ar_finder,new_line]
             rescue SyntaxError => e
               failures << "SyntaxError when evaluatiing options for #{ar_finder}"
@@ -54,13 +43,51 @@ module Rails
             end
           end.compact
           alert(file, new_ar_finders, failures) unless (new_ar_finders.nil? || new_ar_finders.empty?) && failures.empty?
+          # File.open(File.expand_path(file), 'w') {|f| f.puts file_contents }
         end
       end
       
+      def process_line(finder)
+        case
+        when finder.include?('find')
+          convert_find(finder)
+        when finder.include?('all('), finder.include?('first(')
+          convert_all_or_first(finder)
+        end
+      end
+    
     protected
     
-      def find_method(line)
-        i = line.index('find')
+      def convert_find(finder)
+        full_method, arguments = extract_method(finder)
+        case full_method
+        when nil
+          raise RuntimeError, "can't parse due to unmatched braces"
+        when 'find(:all)'
+          new_line = finder.sub(full_method, 'all')
+        when 'find(:first)'
+          new_line = finder.sub(full_method, 'first')
+        else
+          finder_hash = arguments.split(',')
+          all_or_first = finder_hash.shift
+          arel = ArelConverter.translate("{#{finder_hash.join(',')}}")
+          new_line = finder.sub(full_method, arel)
+          new_line += ".first" if all_or_first.include?(':first')
+        end
+        new_line
+      end
+      
+      def convert_all_or_first(finder)
+        full_method, arguments = extract_method(finder)
+        finder_hash = arguments.split(',')
+        arel = ArelConverter.translate("{#{finder_hash.join(',')}}")
+        new_line = finder.sub(full_method, arel)
+        new_line += ".first" if full_method =~ /^first/
+        new_line
+      end
+    
+      def extract_method(line)
+        i = line.index('find') || line.index('all(') || line.index('first(')
         braces = 0
         full_method = ''
         args = ''
---
+++ test/active_record_finder_converter_test.rb
@@ -0,0 +1,142 @@
+require 'test_helper'
+require 'active_record_finder_converter'
+require 'fileutils'
+
+tmp_dir = "#{File.dirname(__FILE__)}/fixtures/tmp"
+
+if defined? BASE_ROOT
+  BASE_ROOT.replace tmp_dir
+else
+  BASE_ROOT = tmp_dir
+end
+FileUtils.mkdir_p BASE_ROOT
+
+# Stub out methods on converter class
+module Rails
+  module Converter
+    class Base
+      attr_reader :alerts
+
+      def alert(title, text, more_info_url, culprits)
+        @alerts[title] = [text, more_info_url, culprits]
+      end
+    end
+  end
+end
+
+class ActiveRecordFinderConverterTest < ActiveSupport::TestCase
+  def setup
+    @converter = Rails::Converter::ActiveRecordFinder.new
+    @old_dir = Dir.pwd
+
+    Dir.chdir(BASE_ROOT)
+  end
+
+  def test_convert_straight_find_all
+    converted = @converter.process_line('Post.find(:all)')
+
+    assert_equal converted, "Post.all"
+  end
+
+  def test_convert_straight_find_first
+    converted = @converter.process_line('Post.find(:first)')
+
+    assert_equal converted, "Post.first"
+  end
+
+  def test_convert_all_without_conditions
+    converted = @converter.process_line("@model = Model.all(:select => 'column', :group => 'group_column', :order => 'created_at DESC')")
+
+    assert_equal converted, '@model = Model.select("column").group("group_column").order("created_at DESC")'
+  end
+
+  def test_convert_all_with_conditions_array
+    converted = @converter.process_line("@model = Model.all(:conditions => ['column IS NOT NULL'])")
+
+    assert_equal converted, '@model = Model.where(["column IS NOT NULL"])'
+  end
+
+  def test_convert_all_with_conditions_string
+    converted = @converter.process_line("@model = Model.all(:conditions => 'column IS NOT NULL')")
+
+    assert_equal converted, '@model = Model.where("column IS NOT NULL")'
+  end
+
+  def test_convert_all_with_conditions_hash
+    converted = @converter.process_line("@model = Model.all(:conditions => {:status => 'active', :current_user_id => current_user, :enabled => true})")
+
+    assert_equal converted, '@model = Model.where({ :status => "active", :current_user_id => (current_user), :enabled => true })'
+  end
+
+  def test_convert_first_without_conditions
+    converted = @converter.process_line("@model = Model.first(:select => 'column', :group => 'group_column', :order => 'created_at DESC')")
+
+    assert_equal converted, '@model = Model.select("column").group("group_column").order("created_at DESC").first'
+  end
+
+  def test_convert_first_with_conditions_array
+    converted = @converter.process_line("@model = Model.first(:conditions => ['column IS NOT NULL'])")
+
+    assert_equal converted, '@model = Model.where(["column IS NOT NULL"]).first'
+  end
+
+  def test_convert_first_with_conditions_string
+    converted = @converter.process_line("@model = Model.first(:conditions => 'column IS NOT NULL')")
+
+    assert_equal converted, '@model = Model.where("column IS NOT NULL").first'
+  end
+
+  def test_convert_first_with_conditions_hash
+    converted = @converter.process_line("@model = Model.first(:conditions => {:status => 'active', :current_user_id => current_user, :enabled => true})")
+
+    assert_equal converted, '@model = Model.where({ :status => "active", :current_user_id => (current_user), :enabled => true }).first'
+  end
+
+  def test_convert_find_all_without_conditions
+    converted = @converter.process_line("@model = Model.find(:all, :select => 'column', :group => 'group_column', :order => 'created_at DESC')")
+
+    assert_equal converted, '@model = Model.select("column").group("group_column").order("created_at DESC")'
+  end
+
+  def test_convert_find_all_with_conditions_array
+    converted = @converter.process_line("@model = Model.find(:all, :conditions => ['column IS NOT NULL'])")
+
+    assert_equal converted, '@model = Model.where(["column IS NOT NULL"])'
+  end
+
+  def test_convert_find_all_with_conditions_string
+    converted = @converter.process_line("@model = Model.find(:all, :conditions => 'column IS NOT NULL')")
+
+    assert_equal converted, '@model = Model.where("column IS NOT NULL")'
+  end
+
+  def test_convert_find_all_with_conditions_hash
+    converted = @converter.process_line("@model = Model.find(:all, :conditions => {:status => 'active', :current_user_id => current_user, :enabled => true})")
+
+    assert_equal converted, '@model = Model.where({ :status => "active", :current_user_id => (current_user), :enabled => true })'
+  end
+
+  def test_convert_keeps_chained_methods
+    converted = @converter.process_line("@model = Model.find(:all, :conditions => {:status => 'active'}).map(&:name)")
+
+    assert_equal converted, '@model = Model.where({ :status => "active" }).map(&:name)'
+  end
+
+  
+  def teardown
+    clear_files
+
+    Dir.chdir(@old_dir)
+  end
+
+  def make_file(where, name=nil, contents=nil)
+    FileUtils.mkdir_p "#{BASE_ROOT}/#{where}"
+    File.open("#{BASE_ROOT}/#{where}/#{name}", "w+") do |f|
+      f.write(contents)
+    end if name
+  end
+
+  def clear_files
+    FileUtils.rm_rf(Dir.glob("#{BASE_ROOT}/*"))
+  end
+end
